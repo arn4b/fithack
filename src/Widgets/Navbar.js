6869837250a7bf1061efe25b51274e4d3d89de56
ashishmishra-bit <ashish.mishra52002@gmail.com>
2021-02-19 08:25:09 +0530

    navbar created

--- src/Widgets/Navbar.js
+++ src/Widgets/Navbar.js
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, { Component } from 'react';
+import{MenuItem} from "./MenuItem"
+import './Navbar.css'
+import {Button} from "./Button"
 
-function Navbar() {
-    return (
-        <div>
-            
-        </div>
-    )
+class Navbar extends Component {
+    state = {clicked: false}
+
+    handleClick = () => {
+        this.setState({clicked: !this.state.clicked})
+    }
+
+    render() {
+        return (
+            <nav className="NavbarItems">
+                <h1 className = "navbar-logo">FITHACK <i className="fas fa-heartbeat"></i></h1>
+                <div className = "menu-icon" onClick={this.handleClick}>
+                    <i className={this.state.clicked ? 'fas fa-times' : 'fas fa-bars'}></i>
+                </div>
+                <ul className={this.state.clicked ? 'nav-menu active' : 'nav-menu'}>
+                    {MenuItem.map((item, index) => {
+                        return(
+                            <li key={index}><a className={item.cName} href={item.url}>
+                                {item.title}
+                            </a></li>
+                        )
+                    })}                    
+                </ul>
+                <Button>SignUp</Button>
+            </nav>
+        )
+    }
 }
 
-export default Navbar;
+export default Navbar
+
