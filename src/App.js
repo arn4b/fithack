06ff9037876def53b9ec3a116c2cada327c9b099
Arnab Chatterjee <arnabc1701@gmail.com>
2021-02-20 15:41:24 +0530

    login and signup

--- src/App.js
+++ src/App.js
@@ -1,20 +1,36 @@
 import React from 'react';
+import {Link, Route, BrowserRouter, Switch} from "react-router-dom"
 import Navbar from "./Widgets/Navbar";
 import Footer from "./Widgets/Footer";
 import Login from "./Pages/Login";
 import Signup from "./Pages/Signup";
 import './App.css';
 
+const Routing = () => {
+  return(
+    <Switch>
+      <Route path="/login">
+        <Login />
+      </Route>
+      <Route path="/signup">
+        <Signup />
+      </Route>
+    </Switch>
+  )
+}
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Navbar/>
       <div>
         <h1 align="center">Hello</h1>
       </div>
+      <Routing />
       <Login />
       <Signup />
       <Footer />
+      </BrowserRouter>
     </div>
   );
 }
